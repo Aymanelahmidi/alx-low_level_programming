3014e22a4d480224fb41407be345c78edf3009f7
Aymanelahmidi <aymanelahmidi928@gmail.com>
2023-10-05 14:15:27 -0700

    my commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,25 +1,32 @@
 #include "main.h"
 
 /**
- * read_textfile - reads text from a file and prints it
- * @filename: name of file to read 
- * @letters: number of bytes to read
- *
- * Return: number bytes read/printed
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: pointer to text in a file
+ * @letters: number of letters
+ * Return: the actual number of letters it could read and print
  */
-ssize_t read_textfule(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t bytes;
-	char buf[READ_BUF_SIZE * 8];
+    ssize_t file, fread, fwrite;
+    char *totalSize;
 
-	if (!filename || !letters)
-		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	bytes = read(fd, &buf[0], letters);
-	bytes = write(STDOUT_FILENO, &buf[0], bytes);
-	close(fd);
-	return (bytes);
+    totalSize = malloc(sizeof(char) * letters);
+    if (totalSize == NULL)
+        return (0);
+    if (filename == NULL)
+        return (0);
+
+    file = open(filename, O_RDONLY);
+    if (file == -1)
+        return (0);
+    fread = read(file, totalSize, letters);
+    if (fread == -1)
+        return (0);
+    fwrite = write(STDOUT_FILENO, totalSize, fread);
+    if (fwrite == -1)
+        return (0);
+    close(file);
+    free(totalSize);
+    return (fwrite);
 }
